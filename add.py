8c086c38d09efa1525c5995a720b499c11636557
24pa1a05a6-max <24pa1a05a6@vishnu.edu.in>
2026-07-14 15:07:44 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,5 @@
+import numpy as np
+x=np.array([1,2,3])
+print(x)
+y=np.append(x,[4,5,6])
+print(y)
---
+++ remove.py
@@ -0,0 +1,7 @@
+import numpy as np
+x=np.array([1,2,3])
+print("Elements before deleting :")
+print(x)
+newarray=np.delete(x,1)
+print("Elements after deleting :")
+print(newarray)
