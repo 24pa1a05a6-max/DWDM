3683960f4afd4bba154c28a67e9a8295cb0772d7
24pa1a05a6-max <24pa1a05a6@vishnu.edu.in>
2026-07-14 15:52:04 +0530

    Add files via upload

---
+++ slice.py
@@ -0,0 +1,8 @@
+import numpy as np
+a=np.arange(10)**3
+print("lets consider the elements",a)
+print("element at a[2] is",a[2])
+print("outputof a[2:4] is",a[2:4])
+print("output of a[-1]",a[-1])
+print("output of a[-2]",a[-2])
+print("outputof a[-5:-2]",a[-5:-2])
